d364f9df7e84bd2a6dceda266481c8e949ec51e8
Ales Ferlan <ales.ferlan@infotrans.si>
2018-04-05 12:09:53 +0100

    Convert lib to DotNet Standard and create sample Core and Framework projects

---
+++ framework/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using DataFileReader;
+
+namespace tachograph_reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            settings.IndentChars = "\t";
+
+            FileStream fileStream_m = new FileStream("m_file.xml", FileMode.Create);
+            XmlWriter writer_m = XmlWriter.Create(fileStream_m, settings);
+
+
+            DataFile vudf = VehicleUnitDataFile.Create();
+
+            //dcdf.Process("samples/ddd/C_20170713_1355_P_MILOSEVIC_SRBSRB0000013490000.DDD", writer_c);
+            vudf.Process("C:/cygwin64/home/ales.ferlan/Development/samples/ddd/M_20170303_1354_WDB96340610122689_20161203_20170303.DDD", writer_m);
+
+
+            writer_m.Flush();
+            fileStream_m.Flush();
+
+            writer_m.Close();
+            fileStream_m.Close();
+
+        }
+    }
+}
--- standard/DataFile.cs
+++ standard/DataFile.cs
@@ -462,78 +462,43 @@ namespace DataFileReader
 
 	}
 
-	// A string that is prefixed by a code page byte
-	public class CodePageStringRegion : SimpleStringRegion
-	{
-		static Dictionary<string, Encoding> encodingCache = new Dictionary<string, Encoding>();
-		static Dictionary<byte, string> charsetMapping = new Dictionary<byte, string>();
+    // A string that is prefixed by a code page byte
+    // TODO: H: the codepages in the data are not valid code pages for windows
+    public class CodePageStringRegion : SimpleStringRegion
+    {
+        private int codepage;
 
-		// private int codepage;
+        public CodePageStringRegion()
+        {
+        }
 
-		static CodePageStringRegion() {
-			foreach ( var i in Encoding.GetEncodings() ) {
-				encodingCache.Add(i.Name.ToUpper(), i.GetEncoding());
-			}
+        public CodePageStringRegion(int size) : base(size)
+        {
+        }
 
-			charsetMapping[0] = "ASCII";
-			charsetMapping[1] = "ISO-8859-1";
-			charsetMapping[2] = "ISO-8859-2";
-			charsetMapping[3] = "ISO-8859-3";
-			charsetMapping[5] = "ISO-8859-5";
-			charsetMapping[7] = "ISO-8859-7";
-			charsetMapping[9] = "ISO-8859-9";
-			charsetMapping[13] = "ISO-8859-13";
-			charsetMapping[15] = "ISO-8859-15";
-			charsetMapping[16] = "ISO-8859-16";
-			charsetMapping[80] = "KOI8-R";
-			charsetMapping[85] = "KOI8-U";
+        protected override void ProcessInternal(CustomBinaryReader reader, XmlWriter writer)
+        {
+            // get the codepage
+            codepage = reader.ReadByte();
+            Encoding enc = Encoding.Default;
+            try
+            {
+                // try to get the encoding
+                enc = Encoding.GetEncoding(codepage);
+            }
+            catch (Exception)
+            {
+                // TODO: H: work out what the code page should be
+                // Console.WriteLine("WARN: Failed to work with codepage {0}, '{1}'", codepage, e.Message);
+            }
+            // read string using encoding
+            base.ProcessInternal(reader, enc);
+            writer.WriteString(text);
+        }
+    }
 
-			// CodePagesEncodingProvider (System.Text.Encoding.CodePages package) on .NET Core by default (GetEncodings() method) supports only few encodings
-			// https://msdn.microsoft.com/en-us/library/system.text.codepagesencodingprovider.aspx#Anchor_4
-			// but if you call GetEncoding directly by name you can get other encodings too
-			// so here we add those too to our cache
-			foreach (var encodingName in charsetMapping.Values) {
-				if (!encodingCache.ContainsKey(encodingName)) {
-					try {
-						var encoding = Encoding.GetEncoding(encodingName);
-						encodingCache.Add(encodingName, encoding);
-					} catch (ArgumentException e) {
-						Console.WriteLine("Warning! Current platform doesn't support encoding with name {0}\n{1}", encodingName, e.Message);
-					}
-				}
-			}
-		}
-
-		public CodePageStringRegion()
-		{
-		}
-
-		public CodePageStringRegion(int size) : base(size)
-		{
-		}
-
-		protected override void ProcessInternal(CustomBinaryReader reader, XmlWriter writer)
-		{
-			// get the codepage
-			var codepage=reader.ReadByte();
-			// codePage specifies the part of the ISO/IEC 8859 used to code this string
-
-			string encodingName = charsetMapping.GetValueOrDefault(codepage, "UNKNOWN");
-			Encoding enc=encodingCache.GetValueOrDefault(encodingName, null);
-			if ( enc == null) {
-				// we want to warn if we didn't recognize codepage because using wrong codepage will cause use of wrong codepoints and thus incorrect data
-				WriteLine(LogLevel.WARN, "Unknown codepage {0}", codepage);
-				enc=Encoding.ASCII;
-			}
-
-			// read string using encoding
-			base.ProcessInternal(reader, enc);
-			writer.WriteString(text);
-		}
-	}
-
-	// A name is a string with codepage with fixed length = 35
-	public class NameRegion : CodePageStringRegion
+    // A name is a string with codepage with fixed length = 35
+    public class NameRegion : CodePageStringRegion
 	{
 		private static readonly int SIZE=35;
 
